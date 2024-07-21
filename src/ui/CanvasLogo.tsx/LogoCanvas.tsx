be8bd985034f6bc23214a315aaff67bee4e19c8a
krokosik <wikkra@gmail.com>
2024-07-21 19:03:13 +0200

    Leaner LogoCanvas component

---
+++ src/ui/CanvasLogo.tsx/LogoCanvas.tsx
@@ -0,0 +1,104 @@
+'use client';
+import { Simulation } from 'd3-force';
+import { useEffect, useRef, useState } from 'react';
+
+import {
+  draw,
+  initSimulation,
+  NUM_ORBS,
+  resizeCanvasToDisplaySize,
+  SimulationNode,
+} from './canvas.utils';
+
+export interface SimulationParams {
+  orbRadiiInDim: number;
+  gasDensity: number;
+  temperature: number;
+  maxLinkThicknessPerRadius: number;
+  maxRangePerRadius: number;
+  backgroundColor: string;
+  fillColor: string;
+}
+
+const defaultParams: SimulationParams = {
+  orbRadiiInDim: 8,
+  gasDensity: 0.0001,
+  temperature: 5,
+  maxLinkThicknessPerRadius: 0.6,
+  maxRangePerRadius: 1.7,
+  backgroundColor: '#000',
+  fillColor: '#fff',
+};
+
+export default function LogoCanvas(props: Partial<SimulationParams>) {
+  const params = { ...defaultParams, ...props };
+  const [simulation, setSimulation] =
+    useState<Simulation<SimulationNode, any>>();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const requestIdRef = useRef<number>();
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const canvas = canvasRef.current;
+    resizeCanvasToDisplaySize(canvas);
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    const { width, height } = canvas;
+
+    const orbRadius = Math.min(width, height) / params.orbRadiiInDim;
+    const gasRadius = Math.max(1, Math.sqrt(orbRadius) / 2);
+    const {
+      backgroundColor,
+      fillColor,
+      gasDensity,
+      temperature,
+      maxLinkThicknessPerRadius,
+      maxRangePerRadius,
+    } = params;
+
+    setSimulation(
+      initSimulation(
+        gasDensity,
+        orbRadius,
+        gasRadius,
+        temperature,
+        width,
+        height,
+      ),
+    );
+
+    function drawFrame() {
+      if (!ctx || !simulation) return;
+      simulation?.tick();
+      draw(
+        ctx,
+        NUM_ORBS,
+        orbRadius,
+        { background: backgroundColor, fill: fillColor },
+        maxLinkThicknessPerRadius,
+        maxRangePerRadius,
+        simulation,
+      );
+      requestIdRef.current = requestAnimationFrame(drawFrame);
+    }
+    drawFrame();
+
+    function resizeHandler() {
+      if (requestIdRef.current) {
+        cancelAnimationFrame(requestIdRef.current);
+      }
+      resizeCanvasToDisplaySize(canvas);
+      setSimulation(undefined);
+    }
+    window.addEventListener('resize', resizeHandler);
+
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+    };
+  }, [!simulation]);
+
+  return <canvas ref={canvasRef} className="h-full w-full" />;
+}
---
+++ src/ui/CanvasLogo.tsx/canvas.utils.ts
@@ -0,0 +1,255 @@
+import { forceSimulation, Simulation, SimulationNodeDatum } from 'd3-force';
+import d3ForceBounce from 'd3-force-bounce';
+import d3ForceSurface from 'd3-force-surface';
+
+export interface SimulationNode extends SimulationNodeDatum {
+  type: 'orb' | 'gas';
+  r: number;
+}
+
+export const NUM_ORBS = 5;
+
+export function initSimulation(
+  gasDensity: number,
+  orbRadius: number,
+  gasRadius: number,
+  temperature: number,
+  width: number,
+  height: number,
+) {
+  return forceSimulation(
+    initOrbs(
+      NUM_ORBS,
+      gasDensity,
+      orbRadius,
+      gasRadius,
+      temperature,
+      width,
+      height,
+    ),
+  )
+    .alphaDecay(0)
+    .velocityDecay(0)
+    .force(
+      'bounce',
+      d3ForceBounce().radius((d: SimulationNode) => d.r),
+    )
+    .force(
+      'surface',
+      d3ForceSurface()
+        .surfaces([
+          { from: { x: width, y: 0 }, to: { x: 0, y: 0 } },
+          { from: { x: width, y: height }, to: { x: width, y: 0 } },
+          { from: { x: 0, y: height }, to: { x: width, y: height } },
+          { from: { x: 0, y: 0 }, to: { x: 0, y: height } },
+        ])
+        .oneWay(true)
+        .radius((d: SimulationNode) => d.r),
+    )
+    .on('tick', () => {})
+    .stop();
+}
+
+export function getCanvasPosition(
+  event: MouseEvent,
+  canvas: HTMLCanvasElement,
+) {
+  const rect = canvas.getBoundingClientRect();
+  const scaleX = canvas.width / rect.width;
+  const scaleY = canvas.height / rect.height;
+
+  return {
+    x: (event.clientX - rect.left) * scaleX,
+    y: (event.clientY - rect.top) * scaleY,
+  };
+}
+
+export function resizeCanvasToDisplaySize(canvas: HTMLCanvasElement): void {
+  // Lookup the size the browser is displaying the canvas in CSS pixels.
+  const dpr = window.devicePixelRatio;
+  const displayWidth = Math.round(canvas.clientWidth * dpr);
+  const displayHeight = Math.round(canvas.clientHeight * dpr);
+
+  // Check if the canvas is not the same size.
+  const needResize =
+    canvas.width != displayWidth || canvas.height != displayHeight;
+
+  if (needResize) {
+    // Make the canvas the same size
+    canvas.width = displayWidth;
+    canvas.height = displayHeight;
+  }
+}
+
+export function draw(
+  ctx: CanvasRenderingContext2D,
+  numOrbs: number,
+  orbRadius: number,
+  colors: { background: string; fill: string },
+  maxLinkThicknessPerRadius: number,
+  maxRangePerRadius: number,
+  simulation?: Simulation<SimulationNode, any> | undefined,
+) {
+  const nodes = simulation?.nodes();
+  if (!nodes) {
+    return;
+  }
+  const { background, fill } = colors;
+  ctx.fillStyle = background;
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  ctx.fillStyle = fill;
+  for (const node of nodes.slice(0, numOrbs)) {
+    if (node.type === 'orb') {
+      ctx.beginPath();
+      ctx.arc(node.x!, node.y!, node.r, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+  }
+
+  for (let i = 0; i < numOrbs; i++) {
+    for (let j = i + 1; j < numOrbs; j++) {
+      const source = nodes[i];
+      const target = nodes[j];
+
+      if (!source.x || !source.y || !target.x || !target.y) continue;
+
+      const distance = Math.hypot(source.x - target.x, source.y - target.y);
+
+      const thickness =
+        orbRadius *
+        Math.sqrt(
+          Math.max(
+            0,
+            maxLinkThicknessPerRadius ** 2 -
+              ((distance / orbRadius - 2) / maxRangePerRadius) ** 2,
+          ),
+        );
+
+      if (thickness <= 0 || isNaN(thickness)) continue;
+
+      const angleBetween = Math.atan2(target.y - source.y, target.x - source.x);
+
+      const doubleChordAngle = Math.asin(thickness / orbRadius);
+
+      const angle1 = angleBetween + doubleChordAngle;
+      const angle2 = angleBetween - doubleChordAngle;
+
+      const angle3 = angleBetween + Math.PI - doubleChordAngle;
+      const angle4 = angleBetween + Math.PI + doubleChordAngle;
+
+      const p1 = shiftByAngle(source.x, source.y, angle1, orbRadius);
+      const p2 = shiftByAngle(source.x, source.y, angle2, orbRadius);
+      const p3 = shiftByAngle(target.x, target.y, angle3, orbRadius);
+      const p4 = shiftByAngle(target.x, target.y, angle4, orbRadius);
+
+      const angleDiff = Math.atan(thickness / 2 / orbRadius);
+      const h1 = shiftByAngle(
+        source.x,
+        source.y,
+        angleBetween + angleDiff,
+        orbRadius,
+      );
+      const h2 = shiftByAngle(
+        source.x,
+        source.y,
+        angleBetween - angleDiff,
+        orbRadius,
+      );
+      const h3 = shiftByAngle(
+        target.x,
+        target.y,
+        angleBetween + Math.PI - angleDiff,
+        orbRadius,
+      );
+      const h4 = shiftByAngle(
+        target.x,
+        target.y,
+        angleBetween + Math.PI + angleDiff,
+        orbRadius,
+      );
+
+      const mid1 = shiftByAngle(
+        source.x,
+        source.y,
+        angleBetween + Math.atan(thickness / distance),
+        distance / 2,
+      );
+      const mid2 = shiftByAngle(
+        source.x,
+        source.y,
+        angleBetween - Math.atan(thickness / distance),
+        distance / 2,
+      );
+
+      ctx.beginPath();
+      ctx.moveTo(p1.x, p1.y);
+      ctx.quadraticCurveTo(h1.x, h1.y, mid1.x, mid1.y);
+      ctx.quadraticCurveTo(h3.x, h3.y, p3.x, p3.y);
+      ctx.lineTo(p4.x, p4.y);
+      ctx.quadraticCurveTo(h4.x, h4.y, mid2.x, mid2.y);
+      ctx.quadraticCurveTo(h2.x, h2.y, p2.x, p2.y);
+      ctx.closePath();
+      ctx.fillStyle = fill;
+      ctx.fill();
+    }
+  }
+}
+
+export function initOrbs(
+  numOrbs: number,
+  gasDensity: number,
+  orbRadius: number,
+  gasRadius: number,
+  temperature: number,
+  width: number,
+  height: number,
+): SimulationNode[] {
+  const nodes: SimulationNode[] = [];
+
+  for (let i = 0; i < numOrbs; i++) {
+    nodes.push({
+      type: 'orb',
+      x: random(orbRadius * 1.5, width - orbRadius * 1.5),
+      y: random(orbRadius * 1.5, height - orbRadius * 1.5),
+      vx: 0,
+      vy: 0,
+      r: orbRadius,
+    });
+  }
+
+  const numDustParticales = gasDensity * width * height;
+  if (gasRadius && numDustParticales) {
+    for (let i = 0; i < numDustParticales; i++) {
+      nodes.push({
+        type: 'gas',
+        x: random(gasRadius, width - gasRadius),
+        y: random(gasRadius, height - gasRadius),
+        vx: gaussianRandom(0, temperature),
+        vy: gaussianRandom(0, temperature),
+        r: gasRadius,
+      });
+    }
+  }
+
+  return nodes;
+}
+
+function random(min: number, max: number): number {
+  return Math.random() * (max - min) + min;
+}
+
+function gaussianRandom(mean = 0, stdev = 1): number {
+  const u = 1 - Math.random(); // Converting [0,1) to (0,1]
+  const v = Math.random();
+  const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+  // Transform to the desired mean and standard deviation:
+  return z * stdev + mean;
+}
+
+function shiftByAngle(x: number, y: number, angle: number, distance: number) {
+  return {
+    x: x + Math.cos(angle) * distance,
+    y: y + Math.sin(angle) * distance,
+  };
+}
