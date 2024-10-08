e4baf0e7f1f5f90da52e411cf36f0568ba88beb0
krokosik <wikkra@gmail.com>
2024-10-08 22:54:36 +0200

    Improvements to footer and header

--- src/ui/footer/index.tsx
+++ src/ui/footer/index.tsx
@@ -12,31 +12,30 @@ export default async function Footer() {
 
   return (
     <footer className="bg-ink text-center text-canvas">
-      <div className="section border-b border-canvas/20">
+      <div className="section border-b border-canvas/20 pb-4 pt-6">
         <div className="mx-auto max-w-screen-xl space-y-8">
-          <div className="flex flex-wrap justify-between gap-x-12 gap-y-8">
-            <div className="flex flex-col gap-3 self-start max-sm:mx-auto max-sm:items-center">
-              <Link className="h3 md:h2 max-w-max" href="/">
-                {logoImage ? (
-                  <Img
-                    className="max-h-[1.5em] w-auto"
-                    image={logoImage}
-                    alt={logo?.name || title}
-                  />
-                ) : (
-                  title
-                )}
-              </Link>
+          <div className="flex flex-wrap justify-between gap-y-8">
+            <Link className="h3 md:h2 max-w-max" href="/">
+              {logoImage ? (
+                <Img
+                  className="max-h-[1.5em] w-auto"
+                  image={logoImage}
+                  alt={logo?.name || title}
+                />
+              ) : (
+                title
+              )}
+            </Link>
 
+            <div className="ml-auto flex flex-col items-end gap-3">
+              <Navigation />
               <Social />
             </div>
-
-            <Navigation />
           </div>
         </div>
       </div>
 
-      <div className="flex flex-wrap justify-center gap-x-6 gap-y-2 p-4 text-sm">
+      <div className="flex flex-wrap justify-center gap-x-6 gap-y-2 p-2 text-sm">
         &copy; {new Date().getFullYear()}{' '}
         {copyright ? <CustomPortableText value={copyright} /> : title}
       </div>
