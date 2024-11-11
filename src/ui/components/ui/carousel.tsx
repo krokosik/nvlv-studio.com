b03cfb7ceb846da1726ec6e0bba9aaac1bd6c4a0
krokosik <wikkra@gmail.com>
2024-11-11 17:46:21 +0100

    Carousel autoplay by default

--- src/ui/components/ui/carousel.tsx
+++ src/ui/components/ui/carousel.tsx
@@ -8,6 +8,7 @@ import { ArrowLeft, ArrowRight } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 import { Button } from '@/ui/components/ui/button';
+import Autoplay from 'embla-carousel-autoplay';
 
 type CarouselApi = UseEmblaCarouselType[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
@@ -65,7 +66,14 @@ const Carousel = React.forwardRef<
         ...opts,
         axis: orientation === 'horizontal' ? 'x' : 'y',
       },
-      plugins,
+      [
+        Autoplay({
+          delay: 5000,
+          stopOnMouseEnter: true,
+          stopOnInteraction: true,
+        }),
+        ...(plugins ?? []),
+      ],
     );
     const [canScrollPrev, setCanScrollPrev] = React.useState(false);
     const [canScrollNext, setCanScrollNext] = React.useState(false);
