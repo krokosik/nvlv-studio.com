229c98ade20c0b2598447b66f6e09b068a892721
krokosik <wikkra@gmail.com>
2024-11-27 15:37:51 +0100

    Set max width for project module

--- src/ui/modules/Project.tsx
+++ src/ui/modules/Project.tsx
@@ -37,65 +37,69 @@ export default function Project({
 
   return (
     <section
-      className="mb-[var(--header-height)] grid h-screen max-h-fold w-full grid-cols-32 grid-rows-3 px-20"
+      className="mb-[var(--header-height)] grid h-screen max-h-fold w-full px-20"
       style={{
         backgroundColor: colors.backgroundColor,
         color: colors.textColor,
       }}
     >
-      <div className="col-span-3 col-start-1 row-start-1 mt-12 aspect-square">
-        <LogoCanvas
-          static
-          square
-          fillColor={colors.textColor}
-          backgroundColor="transparent"
-        />
-      </div>
-      <div className="col-span-full col-start-1 row-span-2 row-start-1 grid h-full grid-cols-subgrid divide-x divide-black">
-        <div className="col-span-7" />
-        <div className="col-span-12 p-2">
-          <Carousel opts={{ loop: true }}>
-            <CarouselContent className="items-center">
-              {gallery?.images.map((image) => (
-                <CarouselItem
-                  key={image._key}
-                  className={clsx(image._type !== 'youtube' && 'basis-2/3')}
-                >
-                  <figure className="relative max-h-[58svh] w-full">
-                    {image._type === 'youtube' ? (
-                      <YouTubePlayer url={image.url} width="100%" />
-                    ) : (
-                      <Img image={image} />
-                    )}
-                  </figure>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselDots />
-          </Carousel>
+      <div className="mx-auto grid max-w-7xl grid-cols-32 grid-rows-3">
+        <div className="col-span-3 col-start-1 row-start-1 mt-12 aspect-square">
+          <LogoCanvas
+            static
+            square
+            fillColor={colors.textColor}
+            backgroundColor="transparent"
+          />
         </div>
-        <div className="col-span-12 size-full px-4">
-          <div className="flex size-full flex-col justify-center">
-            <CustomPortableText value={description} />
+        <div className="col-span-full col-start-1 row-span-2 row-start-1 grid h-full grid-cols-subgrid divide-x divide-black">
+          <div className="col-span-7" />
+          <div className="col-span-12 p-2">
+            <Carousel opts={{ loop: true }}>
+              <CarouselContent className="items-center">
+                {gallery?.images.map((image) => (
+                  <CarouselItem
+                    key={image._key}
+                    className={clsx(image._type !== 'youtube' && 'basis-2/3')}
+                  >
+                    <figure className="relative max-h-[58svh] w-full">
+                      {image._type === 'youtube' ? (
+                        <YouTubePlayer url={image.url} width="100%" />
+                      ) : (
+                        <Img image={image} />
+                      )}
+                    </figure>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselDots />
+            </Carousel>
           </div>
+          <div className="col-span-12 size-full px-4">
+            <div className="flex size-full flex-col justify-center">
+              <CustomPortableText value={description} />
+            </div>
+          </div>
+          <div className="col-span-1" />
         </div>
-        <div className="col-span-1" />
-      </div>
-      <div className="col-span-full grid grid-cols-subgrid grid-rows-3">
-        <div className="col-span-12 col-start-8 row-span-3 grid size-full grid-rows-subgrid text-6xl font-medium">
-          <span className="self-end" style={{ color: colors.accentColor }}>
-            //:
-          </span>
-          <h3 className="row-span-2">
-            {title?.split(' ')[0]}
-            <span style={{ color: colors.accentColor }}>:</span>
-            <br />
-            {title?.split(' ')[1]}
-            <span style={{ color: colors.accentColor }}>.01</span>
-          </h3>
-        </div>
-        <div className="col-span-12 col-start-20 row-span-2 row-start-2">
-          <Pretitle style={{ color: colors.accentColor }}>{pretitle}</Pretitle>
+        <div className="col-span-full grid grid-cols-subgrid grid-rows-3">
+          <div className="col-span-12 col-start-8 row-span-3 grid size-full grid-rows-subgrid text-6xl font-medium">
+            <span className="self-end" style={{ color: colors.accentColor }}>
+              //:
+            </span>
+            <h3 className="row-span-2">
+              {title?.split(' ')[0]}
+              <span style={{ color: colors.accentColor }}>:</span>
+              <br />
+              {title?.split(' ')[1]}
+              <span style={{ color: colors.accentColor }}>.01</span>
+            </h3>
+          </div>
+          <div className="col-span-12 col-start-20 row-span-2 row-start-2">
+            <Pretitle style={{ color: colors.accentColor }}>
+              {pretitle}
+            </Pretitle>
+          </div>
         </div>
       </div>
     </section>
